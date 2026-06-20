d95101c9921b77b407d73208b94f4e1d698c2261
Abdul Raheem <149375345+abd-RAHEEM@users.noreply.github.com>
2026-06-20 18:03:09 +0530

    Time: 604 ms (34.44%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,13 @@
+SELECT 
+    request_at AS Day,
+    ROUND(
+        SUM(CASE WHEN status != 'completed' THEN 1 ELSE 0 END) / COUNT(*), 
+        2
+    ) AS `Cancellation Rate`
+FROM Trips t
+JOIN Users u1 ON t.client_id = u1.users_id
+JOIN Users u2 ON t.driver_id = u2.users_id
+WHERE u1.banned = 'No' 
+  AND u2.banned = 'No'
+  AND request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY request_at;
