bcef00a96fac5942a43ce757dd447dcdddf45026
Abdul Raheem <149375345+abd-RAHEEM@users.noreply.github.com>
2026-07-26 18:31:57 +0530

    Time: 260 ms (72.3%), Space: 33.5 MB (68.53%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def carFleet(self, target, position, speed):
+        """
+        :type target: int
+        :type position: List[int]
+        :type speed: List[int]
+        :rtype: int
+        """
+        cars=sorted(zip(position,speed),reverse=True)
+        stack=[]
+        for pos,spd in cars:
+            time=float(target-pos)/spd
+            if not stack or time>stack[-1]:
+                stack.append(time)
+        return len(stack)
+        
